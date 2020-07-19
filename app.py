6a4e880200cb01b3f4890b148f9c8140e3cb09d4
stevens-morons <42627085+stevens-morons@users.noreply.github.com>
2020-07-18 20:17:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from binance.enums import *
 from chalicelib import BinanceKeys
 # from BinanceKeys import BinanceKey1
 
-from chalicelib import precision,price_precision_dict,quantity_precision_dict,mintick_dict,ticks_dict
+from chalicelib import precision,price_precision_dict,quantity_precision_dict
 
 percent_of_marginbal=0.1 # Order size as 10% of Margin Balance 
 
@@ -22,17 +22,17 @@ from chalice import Chalice
 app = Chalice(app_name='tradingview-binance-alert')
 
 # Defining precision for Price and Qty, Min Ticks and Ticks For Coins - Add coins when required 
-# price_precision_dict = {"BTCUSDT":2, "BCH":2, "BNBUSDT":2, "DASHUSDT":2, "ETCUSDT":3,"ADAUSDT":5,
-#                         "EOSUSDT":3, "LINKUSDT":3, "XMRUSDT":2, "XTZUSDT":3}
+price_precision_dict = {"BTCUSDT":2, "BCH":2, "BNBUSDT":2, "DASHUSDT":2, "ETCUSDT":3,"ADAUSDT":5,
+                        "EOSUSDT":3, "LINKUSDT":3, "XMRUSDT":2, "XTZUSDT":3}
 
-# quantity_precision_dict = {"BTCUSDT":2, "BCH":2, "BNBUSDT":2, "DASHUSDT":3, "ETCUSDT":2,"ADAUSDT":0,
-#                            "EOSUSDT":1, "LINKUSDT":2, "XMRUSDT":3, "XTZUSDT":1}
+quantity_precision_dict = {"BTCUSDT":2, "BCH":2, "BNBUSDT":2, "DASHUSDT":3, "ETCUSDT":2,"ADAUSDT":0,
+                           "EOSUSDT":1, "LINKUSDT":2, "XMRUSDT":3, "XTZUSDT":1}
 
-# mintick_dict = {"BTCUSDT":0.01, "BCH":0.01, "BNBUSDT":0.001, "DASHUSDT":0.01, "ETCUSDT":0.001,"ADAUSDT":0.00001,
-#                 "EOSUSDT":0.001, "LINKUSDT":0.001, "XMRUSDT":0.01, "XTZUSDT":0.001}
+mintick_dict = {"BTCUSDT":0.01, "BCH":0.01, "BNBUSDT":0.001, "DASHUSDT":0.01, "ETCUSDT":0.001,"ADAUSDT":0.00001,
+                "EOSUSDT":0.001, "LINKUSDT":0.001, "XMRUSDT":0.01, "XTZUSDT":0.001}
 
-# ticks_dict = {"BTCUSDT":3195, "BCH":111, "BNBUSDT":73, "DASHUSDT":28, "ETCUSDT":31,"ADAUSDT":35,
-#               "EOSUSDT":19, "LINKUSDT":18, "XMRUSDT":18, "XTZUSDT":12}
+ticks_dict = {"BTCUSDT":3195, "BCH":111, "BNBUSDT":73, "DASHUSDT":28, "ETCUSDT":31,"ADAUSDT":35,
+              "EOSUSDT":19, "LINKUSDT":18, "XMRUSDT":18, "XTZUSDT":12}
 
 
 @app.route('/binancebot',methods=['POST','GET'])
